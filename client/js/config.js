2168f30aafe6e4f88507829500f2052969af6b66
Andres Macias <andrespmo1666@gmail.com>
2026-07-12 22:40:01 -0500

    Fix: SUPABASE_ANON_KEY real en config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   SUPABASE_URL: 'https://fssandkzjzplyuluwrbq.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_-SIQUAx6XcVo6weaAmYVCA_iHKHMKPu',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzc2FuZGt6anpwbHl1bHV3cmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NTMyNDgsImV4cCI6MjA5ODUyOTI0OH0.3iR0ZoI0TeeWaOantMETnPFNCyXL2FQicRu4QW4BGd4',
   API_BASE_URL: 'http://localhost:3000',
 }
 
