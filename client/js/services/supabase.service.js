149a32740a72628314ffb254011311bd657e0d58
Andres Macias <andrespmo1666@gmail.com>
2026-07-12 23:09:42 -0500

    Switch Supabase from ESM import to UMD script tag to fix module loading

--- client/js/services/supabase.service.js
+++ client/js/services/supabase.service.js
@@ -1,6 +1,5 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import CONFIG from '../config.js'
 
-const supabase = createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY)
+const supabase = window.supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY)
 
 export default supabase
